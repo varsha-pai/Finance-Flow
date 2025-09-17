14693cb5bdc3b540ec4a5ec1a1a1ddede95e5a80
varsha <124172423+varsha-pai@users.noreply.github.com>
2025-09-17 23:56:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
